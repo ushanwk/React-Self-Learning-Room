f837e77707d394bbb66b4a8098879184e76da29f
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-16 23:55:23 +0530

    list group dynamic errors handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,17 +6,17 @@ function ListGroup(){
 
     animals = [];
 
-    if(animals.length == 0){
-        return(
-            <>
-                <h1 className="text-3xl text-green-400 pb-5">Hello</h1>
-                <h2>No item found</h2>
-            </>);
-    }
+    // const message = animals.length == 0 ? <h2>No item found</h2>: null;
+
+    // const getMessage = () => {
+    //     return animals.length == 0 ? <h2>No item found</h2>: null;
+    // }
 
     return (
         <>
             <h1 className="text-3xl text-green-400 pb-5">Hello</h1>
+            {/*{animals.length == 0 ? <h2>No item found</h2>: null}*/}
+            {animals.length == 0 && <h2>No item found</h2>}
             <ul className="list-inside">
                 {
                     animals.map(animal=> <li key={animal}>{animal}</li>)
